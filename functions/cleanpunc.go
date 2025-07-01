7b662dfe5bcbf0ce5c096fcc1bd030de677b7671
ybourazz ybourazz <nite246135@gmail.com>
2025-07-01 13:07:01 +0100

    a

--- functions/cleanpunc.go
+++ functions/cleanpunc.go
@@ -8,8 +8,8 @@ func CleanPunc(slice []string) []string {
 			slice[i] = ""
 			slice = CleanSlice(slice)
 			i--	
-		 } else if i > 1 && IsPunctuation(rune(slice[i][0])) && ((slice[i-2] != "(up," && slice[i-2] != "(low," && slice[i-2] != "(cap,") || (!CheckNumber(slice[i-1][:len(slice[i-1])-1]) && slice[i-1][len(slice[i-1])-1] == ')') &&
-		 slice[i-1] != "(up)" && slice[i-1] != "(cap)" && slice[i-1] != "(low)" && slice[i-1] != "(bin)" && slice[i-1] != "(hex)") {
+		 } else if i > 1 && IsPunctuation(rune(slice[i][0])) && (((slice[i-2] != "(up," && slice[i-2] != "(low," && slice[i-2] != "(cap,") || (!CheckNumber(slice[i-1][:len(slice[i-1])-1]) && slice[i-1][len(slice[i-1])-1] == ')')) &&
+		 slice[i-1] != "(up)" && slice[i-1] != "(cap)" && slice[i-1] != "(low)" && slice[i-1] != "(bin)" && (slice[i-2] != "(hex)") && slice[i-2] != "(up)" && slice[i-2] != "(cap)" && slice[i-2] != "(low)" && slice[i-2] != "(bin)" && slice[i-2] != "(hex)") {
 			slice[i-1] += slice[i]
 			slice[i] = ""
 			slice = CleanSlice(slice)
